225240b8a5dabfaf736bf48b2f71f1d9030fed40
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-10-08 08:51:21 -0700

    strat working on second solution

---
+++ Second_prompt.js
@@ -0,0 +1,14 @@
+// Question #2: Array Deduping
+// Write an algorithm that removes duplicates from an array. Do not use a function like filter() to solve this. Once you have solved the problem, demonstrate how it can be solved with filter(). Solve the problem with and without recursion.
+
+// Example
+// Input: [7, 9, "hi", 12, "hi", 7, 53]
+
+// Output: [7, 9, "hi", 12, 53]
+
+// Questions: For strings does "hi" equal "Hi" or are those two distinct?
+
+//declare the function
+// declare duplicateArray
+// declare empty array res, to store new array
+// for loop where element is not equal to
