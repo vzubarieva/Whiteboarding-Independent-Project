10df174c8a7ac2ef961f0c0dee20932c800c6998
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-10-08 10:16:59 -0700

    add solution for second prompt

--- Second_prompt.js
+++ Second_prompt.js
@@ -8,7 +8,50 @@
 
 // Questions: For strings does "hi" equal "Hi" or are those two distinct?
 
-//declare the function
-// declare duplicateArray
-// declare empty array res, to store new array
-// for loop where element is not equal to
+//declare the function and pass  duplicateArray as an argument
+// declare empty array res, to store result array
+// declare boolean variable false to check if current element is found in res array
+// outer for loop iterates over given array
+//inner loop search for current array element in res array and mark found as true if found and break inner loop
+//if element was not found adding it to res array
+
+const removeDuplicate1 = (duplicateArray) => {
+  const res = [];
+  let found = false;
+  for (let i = 0; i < duplicateArray.length; i++) {
+    found = false;
+    for (let j = 0; j < res.length; j++) {
+      if (duplicateArray[i] === res[j]) {
+        found = true;
+        break;
+      }
+    }
+    if (!found) {
+      res.push(duplicateArray[i]);
+    }
+  }
+  return res;
+};
+
+console.log(removeDuplicate1([7, 9, "hi", 12, "hi", 7, 53, 7]));
+
+//solution with .filter()
+
+const removeDuplicateFilter = (duplicateArray) => {
+  return duplicateArray.filter((element, index) => {
+    console.log(
+      `element: ${element} outer loop index: ${index} inner loop found index: ${duplicateArray.indexOf(
+        element
+      )}`
+    );
+    return index === duplicateArray.indexOf(element);
+  });
+};
+
+//"element: 7 outer loop index: 0 inner loop found index: 0"
+//"element: 9 outer loop index: 1 inner loop found index: 1"
+//"element: hi outer loop index: 2 inner loop found index: 2"
+//"element: 12 outer loop index: 3 inner loop found index: 3"
+//"element: hi outer loop index: 4 inner loop found index: 2"
+
+console.log(removeDuplicateFilter([7, 9, "hi", 12, "hi", 7, 53, 7, 9]));
